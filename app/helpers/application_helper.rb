15044d68180c9510b8723d8d337b358b72d4a45d
rejka permana <permanarejka@gmail.com>
2023-12-15 19:53:26 +0700

    field type updated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,11 @@ module ApplicationHelper
     n = []
     r = Rails.application.routes.named_routes.helper_names
     er = [/rails/, /url/, /turbo/, /new/, /edit/, /\w+((?!s).)_path/]
-    r.each { |s| n << s if er.any? { |e| route.match?(e) } }
+    r.each { |s| n << s unless er.any? { |e| s.match?(e) } }
     return n
+  end
+
+  def navigation_extract(str)
+    str.gsub('_path','').split('_').join(' ').capitalize
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   resources :type_memberships
   resources :fields
   resources :field_types
-  get 'home/index'
+  get "home/index"
+  root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
