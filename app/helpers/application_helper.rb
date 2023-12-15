b5ffe9c7829dd6562a628e47b19fb778faeecb3f
rejka permana <permanarejka@gmail.com>
2023-12-15 12:22:35 +0700

    navigation helper created

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def navigation
+    n = []
+    r = Rails.application.routes.named_routes.helper_names
+    er = [/rails/, /url/, /turbo/, /new/, /edit/, /\w+((?!s).)_path/]
+    r.each { |s| n << s if er.any? { |e| route.match?(e) } }
+    return n
+  end
 end
