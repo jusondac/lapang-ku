c2dbe21c5fec9c998915a391262d9c15c8c8f074
rejka permana <permanarejka@gmail.com>
2023-12-18 09:38:36 +0700

    update rent detail seed

--- db/seeds/07_rent_detail.rb
+++ db/seeds/07_rent_detail.rb
@@ -1,14 +1,19 @@
 if RentDetail.all.blank?
-  compare_data = [
-    "Membership.all.sample",
-    "",
-  ]
-  27.times {
+end
+
+unless Rent.all.blank?
+  Rent.all.each do |rent|
+    compare_data = [
+      "Membership.all.sample",
+      "",
+    ]
+
     data = eval(compare_data[rand(0..1)])
     RentDetail.create(
       hours: rand(1..4),
       membership_id: data&.id,
-      behalf: data.nil? ? Faker::Name.name : data.name
+      behalf: data.nil? ? Faker::Name.name : data.name,
+      rent_id: rent.id,
     )
-  }
+  end
 end
