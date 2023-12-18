d2a8bb8f883b5a78b54621c42b48c308f2dea02c
rejka permana <permanarejka@gmail.com>
2023-12-18 10:03:51 +0700

    update rent model

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -2,6 +2,7 @@ class Rent < ApplicationRecord
   scope :total_rent, -> { all.count }
   # Ex:- scope :active, -> {where(:active => true)}
   belongs_to :field
+  has_one :rent_detail
   has_many :rent_membership, foreign_key: :rent_id
 
   def self.not_on_rent(date, field)
