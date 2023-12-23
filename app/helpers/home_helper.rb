860d28049e40fd4bf133c4d699d45229bf323a39
rejka permana <permanarejka@gmail.com>
2023-12-23 12:38:03 +0700

    update footer and navbar

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,5 +1,7 @@
 module HomeHelper
   def on_book(time)
-    time
+    byebug if time.eql?(Time.parse("11:00", Date.parse("23 12 2003")))
+    rent = Rent.where(rent_date: time)
+    rent.blank? ? "bg-green-200" : "bg-red-200"
   end
 end
