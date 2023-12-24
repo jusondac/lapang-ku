96ff3d5fbe21e3a74a950ce827e54c395c4984a1
rejka permana <permanarejka@gmail.com>
2023-12-24 18:49:54 +0700

    update navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,42 +2,29 @@ module ApplicationHelper
   include Pagy::Frontend
 
   def navigation
-    navigation = []
-    # routes = Rails.application.routes.named_routes.helper_names
-    # except_routes = [/rails/, /url/, /turbo/, /new/, /edit/, /\w+((?!s).)_path/]
-    # routes.each { |s| navigation << s unless except_routes.any? { |e| s.match?(e) } }
-    # navigation.map(&:to_sym)
-    # navigation_alias = navigation.map { |x| x.gsub("_", " ").gsub("path", "").capitalize }
-    # navigation = Hash[[navigation_alias, navigation].transpose]
-    # {:Beranda=>"root_path", "Order details "=>"order_details_path", .. }
-    navigation = add_nav(navigation)
-    # navigation = rename_nav(navigation)
-    return navigation
-  end
-
-  def rename_nav(navigation)
-    # navigation = navigation.transform_keys({
- # wait for code
-      # })
-    # return navigation
-  end
-
-  def add_nav(navigation)
-    navigation_addition = {
+    # 
+    # "name": "path"
+    # or
+    # "name_a":{
+    #   "name_a1":"path_a1"
+    # }
+    #
+    navigation = {
       "Beranda": "root_path",
-      "Pembayaran": "payments_path",
-      "Lapang": "fields_path",
-      "Membership": {
-        "List Membership":"memberships_path",
-        "Jenis Membership":"type_memberships_path"
+      "Lapang":{
+        "List Lapang":"fields_path",
+        "Jenis Lapang":"field_types_path"
       },
+      "Membership": {
+        "List Membership": "memberships_path",
+        "Jenis Membership": "type_memberships_path",
+      },
+      "Sewa":"rents_path",
+      "Order":{
+        "List Order":"orders_path",
+        "Pembayaran":"payments_path",
+      }
     }
-    navigation = navigation.inject(navigation_addition) { |h, (k, v)| h[k] = v; h }
-    return navigation_addition
-  end
-
-  def navigation_extract(str)
-    str.gsub("_path", "").split("_").join(" ").capitalize
   end
 
   def status_bg(num)
@@ -56,8 +43,8 @@ module ApplicationHelper
     start = date.beginning_of_day
     finish = date.end_of_day
     hours = []
-    while start <= finish do
-      hours << start 
+    while start <= finish
+      hours << start
       start += 1.hours
     end
     return hours
